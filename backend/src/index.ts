74906f3153dd0f6ef5a9b4b752580c616b209efb
muskanrajputsingh <123744013+muskanrajputsingh@users.noreply.github.com>
2025-09-08 22:46:30 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ const port = process.env.PORT || 12000;
 app.use(
   cors({ 
     origin: ["http://localhost:5173", "http://localhost:5174", "https://sthaannix-real-estate.vercel.app", "https://sthaannix-real-estate2-sand.vercel.app"],
-    credentials: false, 
+    credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"], 
   })
