5f0fdb66e700646812c53c36b2aebf1ef3746a79
muskanrajputsingh <123744013+muskanrajputsingh@users.noreply.github.com>
2025-09-10 20:42:53 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,30 +26,44 @@ const port = process.env.PORT || 12000;
 //     allowedHeaders: ["Content-Type", "Authorization"], 
 //   })
 // );
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "https://sthaannix-real-estate.vercel.app",
-        "https://sthaannix-real-estate2-sand.vercel.app",
-        "https://sthaannix-real-estate2-7q9v.vercel.app",
-      ];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, origin);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       const allowedOrigins = [
+//         "http://localhost:5173",
+//         "http://localhost:5174",
+//         "https://sthaannix-real-estate.vercel.app",
+//         "https://sthaannix-real-estate2-sand.vercel.app",
+//         "https://sthaannix-real-estate2-7q9v.vercel.app",
+//       ];
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, origin);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 
-// Handle preflight requests
-app.options("*", cors());
+// // Handle preflight requests
+// app.options("*", cors());
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://sthaannix-real-estate.vercel.app",
+    "https://sthaannix-real-estate2-sand.vercel.app",
+    "https://sthaannix-real-estate2-7q9v.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
