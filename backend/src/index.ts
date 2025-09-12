58c5aee6cb8f1118a31729907fd96bd96bd9f2de
muskanrajputsingh <123744013+muskanrajputsingh@users.noreply.github.com>
2025-09-12 13:49:26 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -126,39 +126,39 @@ import contactRoutes from "./routes/contactRoutes";
 
 const app = express();
 const port = process.env.PORT || 12000;
-app.use(
-  cors({ 
-    origin: ["http://localhost:5173", "http://localhost:5174",
-       "https://sthaannix-real-estate.vercel.app", "https://sthaannix-real-estate2-sand.vercel.app"],
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"], 
-  })
-);
+// app.use(
+//   cors({ 
+//     origin: ["http://localhost:5173", "http://localhost:5174",
+//        "https://sthaannix-real-estate.vercel.app", "https://sthaannix-real-estate2-sand.vercel.app"],
+//     credentials: true, 
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type", "Authorization"], 
+//   })
+// );
 
 
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "http://localhost:5174",
-//   "https://sthaannix-real-estate2-sand.vercel.app",
-//   "https://sthaannix-real-estate2-7q9v.vercel.app",
-// ];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://sthaannix-real-estate2-sand.vercel.app",
+  "https://sthaannix-real-estate2-7q9v.vercel.app",
+];
 
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);   // ✅ allow
-//     } else {
-//       callback(new Error("Not allowed by CORS: " + origin));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);   // ✅ allow
+    } else {
+      callback(new Error("Not allowed by CORS: " + origin));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 
-// // Explicitly handle preflight
-// app.options("*", cors());
+// Explicitly handle preflight
+app.options("*", cors());
 
 
 
